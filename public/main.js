02bd3a5cfa4b770bde7b9c7239517c0c22d360bf
carmit vaknin <carmit.vk@gmail.com>
2022-01-28 15:22:06 +0200

    זמנים

--- public/main.js
+++ public/main.js
@@ -1394,17 +1394,17 @@ const MOCK_VIEWS_DATA = [
         durationSec: 129600,
         viewType: 'two-boards-view',
         viewFields: {
-            title: 'פרשת יתרו',
+            title: 'פרשת משפטים',
             rightBoard: [
                 {
                     title: 'תפילות שבת',
                     rows: [
-                        { title: 'מנחה ערב שבת', value: '16:50' },
+                        { title: 'מנחה ערב שבת', value: '16:55' },
                         { title: 'שחרית שבת', value: '08:30' },
-                        { title: 'שיעור פרשת שבוע', value: '15:00' },
-                        { title: 'שיעור מסכת מגילה', value: '15:30' },
-                        { title: 'מנחה שבת', value: '16:30' },
-                        { title: 'ערבית מוצ"ש', value: '17:40' },
+                        { title: 'שיעור פרשת שבוע', value: '15:05' },
+                        { title: 'שיעור מסכת מגילה', value: '15:35' },
+                        { title: 'מנחה שבת', value: '16:35' },
+                        { title: 'ערבית מוצ"ש', value: '17:45' },
                     ],
                     durationSec: -1,
                     type: 'time&text'
@@ -1421,7 +1421,7 @@ const MOCK_VIEWS_DATA = [
                     rows: [
                         { title: 'שחרית מנין ראשון', value: '07:00' },
                         { title: 'שחרית מנין שני', value: '08:30' },
-                        { title: 'מנחה וערבית', value: '17:00' },
+                        { title: 'מנחה וערבית', value: '17:05' },
                         { title: '--------', value: '--------' },
                         { title: 'יום שני - ספר התניא', value: '21:00' },
                         { title: 'יום שלישי - מכות', value: '21:00' },
@@ -1432,11 +1432,11 @@ const MOCK_VIEWS_DATA = [
                 {
                     title: 'זמני השבוע',
                     rows: [
-                        { title: 'הנץ החמה', value: '06:47' },
-                        { title: 'סוף ק"ש למ"א', value: '08:38' },
-                        { title: 'סוף ק"ש לגר"א', value: '09:15' },
-                        { title: 'זמן מנחה גדולה', value: '12:23' },
-                        { title: 'שקיעת החמה', value: '17:06' },
+                        { title: 'הנץ החמה', value: '06:44' },
+                        { title: 'סוף ק"ש למ"א', value: '08:37' },
+                        { title: 'סוף ק"ש לגר"א', value: '09:13' },
+                        { title: 'זמן מנחה גדולה', value: '12:24' },
+                        { title: 'שקיעת החמה', value: '17:13' },
                     ],
                     durationSec: 15,
                     type: 'time&text'
@@ -1463,7 +1463,7 @@ const MOCK_VIEWS_DATA = [
                     rows: [
                         { title: 'שחרית מנין ראשון', value: '07:00' },
                         { title: 'שחרית מנין שני', value: '08:30' },
-                        { title: 'מנחה וערבית', value: '17:00' },
+                        { title: 'מנחה וערבית', value: '17:05' },
                         { title: '--------', value: '--------' },
                         { title: 'יום שני - ספר התניא', value: '21:00' },
                         { title: 'יום שלישי - מכות', value: '21:00' },
@@ -1481,11 +1481,11 @@ const MOCK_VIEWS_DATA = [
                 {
                     title: 'זמני השבוע',
                     rows: [
-                        { title: 'הנץ החמה', value: '06:47' },
-                        { title: 'סוף ק"ש למ"א', value: '08:38' },
-                        { title: 'סוף ק"ש לגר"א', value: '09:15' },
-                        { title: 'זמן מנחה גדולה', value: '12:23' },
-                        { title: 'שקיעת החמה', value: '17:06' },
+                        { title: 'הנץ החמה', value: '06:44' },
+                        { title: 'סוף ק"ש למ"א', value: '08:37' },
+                        { title: 'סוף ק"ש לגר"א', value: '09:13' },
+                        { title: 'זמן מנחה גדולה', value: '12:24' },
+                        { title: 'שקיעת החמה', value: '17:13' },
                     ],
                     durationSec: 30,
                     type: 'time&text'
