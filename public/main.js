503b3483185252505315ecf878bbff9e36b81fe8
carmit vaknin <carmit.vk@gmail.com>
2022-02-18 11:14:20 +0200

    times

--- public/main.js
+++ public/main.js
@@ -1394,17 +1394,17 @@ const MOCK_VIEWS_DATA = [
         durationSec: 129600,
         viewType: 'two-boards-view',
         viewFields: {
-            title: 'פרשת תצוה',
+            title: 'פרשת כי תשא',
             rightBoard: [
                 {
                     title: 'תפילות שבת',
                     rows: [
-                        { title: 'מנחה ערב שבת', value: '17:05' },
+                        { title: 'מנחה ערב שבת', value: '17:15' },
                         { title: 'שחרית שבת', value: '08:30' },
-                        { title: 'שיעור פרשת שבוע', value: '15:15' },
-                        { title: 'שיעור מסכת מגילה', value: '15:45' },
-                        { title: 'מנחה שבת', value: '16:45' },
-                        { title: 'ערבית מוצ"ש', value: '17:55' },
+                        { title: 'שיעור פרשת שבוע', value: '15:25' },
+                        { title: 'שיעור מסכת מגילה', value: '15:55' },
+                        { title: 'מנחה שבת', value: '16:55' },
+                        { title: 'ערבית מוצ"ש', value: '18:00' },
                     ],
                     durationSec: -1,
                     type: 'time&text'
@@ -1432,11 +1432,11 @@ const MOCK_VIEWS_DATA = [
                 {
                     title: 'זמני השבוע',
                     rows: [
-                        { title: 'הנץ החמה', value: '06:33' },
-                        { title: 'סוף ק"ש למ"א', value: '08:31' },
-                        { title: 'סוף ק"ש לגר"א', value: '09:07' },
+                        { title: 'הנץ החמה', value: '06:26' },
+                        { title: 'סוף ק"ש למ"א', value: '08:27' },
+                        { title: 'סוף ק"ש לגר"א', value: '09:03' },
                         { title: 'זמן מנחה גדולה', value: '12:25' },
-                        { title: 'שקיעת החמה', value: '17:25' },
+                        { title: 'שקיעת החמה', value: '17:31' },
                     ],
                     durationSec: 15,
                     type: 'time&text'
@@ -1481,11 +1481,11 @@ const MOCK_VIEWS_DATA = [
                 {
                     title: 'זמני השבוע',
                     rows: [
-                        { title: 'הנץ החמה', value: '06:33' },
-                        { title: 'סוף ק"ש למ"א', value: '08:31' },
-                        { title: 'סוף ק"ש לגר"א', value: '09:07' },
+                        { title: 'הנץ החמה', value: '06:26' },
+                        { title: 'סוף ק"ש למ"א', value: '08:27' },
+                        { title: 'סוף ק"ש לגר"א', value: '09:03' },
                         { title: 'זמן מנחה גדולה', value: '12:25' },
-                        { title: 'שקיעת החמה', value: '17:25' },
+                        { title: 'שקיעת החמה', value: '17:31' },
                     ],
                     durationSec: 30,
                     type: 'time&text'
