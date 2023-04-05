d849236133750ee658ff989155def2e013fbacad
carmit vaknin <carmit.vk@gmail.com>
2023-04-05 18:09:24 +0300

    izkor changes

--- synagogue-ui/src/app/pages/izkor-view/izkor-view.component.ts
+++ synagogue-ui/src/app/pages/izkor-view/izkor-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { BehaviorSubject, Subscription, timer } from 'rxjs';
 import { take, tap } from 'rxjs/operators';
 import { IzkorPerson, izkorView } from 'src/app/models/view.interface-izkor';
@@ -14,6 +14,51 @@ export class IzkorViewComponent implements OnInit {
   // private idx:number;
   @Input() view: izkorView;
 
+  /////
+  @ViewChild('scrollframe', {static: false}) scrollFrame: ElementRef;
+  @ViewChildren('item') itemElements: QueryList<any>;
+  
+  // private itemContainer: any;
+  private scrollContainer: any;
+  private items = [];
+  // private isNearBottom = true;
+
+  ngAfterViewInit() {
+    this.scrollContainer = this.scrollFrame.nativeElement;
+    this.itemElements.changes.subscribe(_ => this.onItemElementsChanged());    
+
+    // Add a new item every 2 seconds 
+    setInterval(() => {
+      this.items.push({});
+    }, 2000);
+  }
+  
+  private onItemElementsChanged(): void {
+    // if (this.isNearBottom) {
+      this.scrollToBottom();
+    // }
+  }
+
+  private scrollToBottom(): void {
+    this.scrollContainer.scroll({
+      top: this.scrollContainer.scrollHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  // private isUserNearBottom(): boolean {
+  //   const threshold = 500;
+  //   const position = this.scrollContainer.scrollTop + this.scrollContainer.offsetHeight;
+  //   const height = this.scrollContainer.scrollHeight;
+  //   return position > height - threshold;
+  // }
+
+  // scrolled(event: any): void {
+  //   this.isNearBottom = this.isUserNearBottom();
+  // }
+  /////
+
   constructor() {
     //
     this.view = {
@@ -25,7 +70,7 @@ export class IzkorViewComponent implements OnInit {
       izkorPersons: [
         {
           img: 'candle-small.gif',
-          name: '  בן שלומית 1אברהם בוזגלו',
+          name: 'בן שלומית 1אברהם בוזגלו',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
@@ -43,7 +88,7 @@ export class IzkorViewComponent implements OnInit {
         },
         {
           img: 'candle-small.gif',
-          name: '4דן אלפסי',
+          name: '4יוחאי וענונו בן שרה',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
@@ -52,88 +97,231 @@ export class IzkorViewComponent implements OnInit {
           name: '5דן אלפסי',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
-        },
-        {
+        },{
           img: 'candle-small.gif',
-          name: '6דן אלפסי',
+          name: '6יוחאי וענונו בן שרה',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '7דן אלפסי',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '8יוחאי וענונו בן שרה',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '9דן אלפסי',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '10יוחאי וענונו בן שרה',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '11דן אלפסי',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '12יוחאי וענונו בן שרה',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '13יוחאי וענונו בן שרה',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '14דן אלפסי',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '15יוחאי וענונו בן שרה',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '16דן אלפסי',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '17יוחאי וענונו בן שרה',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '18דן אלפסי',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
         {
           img: 'candle-small.gif',
-          name: 'דן אלפסי',
+          name: '19יוחאי וענונו בן שרה',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '20יוחאי וענונו בן שרה',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '21דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '22יוחאי וענונו בן שרה',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '23דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '24דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '25דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '26יוחאי וענונו בן שרה',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '27דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '28דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '29דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '30דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '31דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '32דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '33דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '34דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '35דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '36דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '37דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '38דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '39דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '40דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '41דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '42דן אלפסי',
+          date: 'כז חשון תשפ"א',
+          footer: 'ת.נ.צ.ב.ה'
+        },
+        {
+          img: 'candle-small.gif',
+          name: '43דן אלפסי',
           date: 'כז חשון תשפ"א',
           footer: 'ת.נ.צ.ב.ה'
         },
