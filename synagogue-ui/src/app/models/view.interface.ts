9ef6a19e545d0cbd2c5bc15209fb8bdb6469b29b
carmit vaknin <carmit.vk@gmail.com>
2023-06-02 14:48:39 +0300

    update times

--- synagogue-ui/src/app/models/view.interface.ts
+++ synagogue-ui/src/app/models/view.interface.ts
@@ -34,68 +34,6 @@ export interface ViewFields {
 
 export const MOCK_VIEWS_DATA: Array<View> = [
   {
-    viewName: 'תצוגה לשבועות',
-    durationSec: 129600,
-    viewType: 'two-boards-view',
-    viewFields: {
-      title: 'חג השבועות',
-      rightBoard: [
-        {
-          title: 'זמני החג',
-          rows: [
-            { title: 'לימוד ליל שבועות', value: '23:00' },
-            { title: 'שחרית מניין ראשון', value: '05:20' },
-            { title: 'שחרית מניין שני', value: '08:30' },
-            { title: 'מגילת רות לנשים', value: '17:30' },
-            { title: 'מנחה ערב שבת', value: '19:30' },
-          ],
-          durationSec: -1,
-          type: 'time&text'
-        }
-      ],
-      leftBoard: [
-        {
-          title: 'shavuot.webp',
-          type: 'image',
-          durationSec: 15,
-        }, 
-        {
-          title: 'זמני השבת',
-          rows: [
-            { title: 'שחרית שבת', value: '08:30' },
-            { title: 'תהילים לילדים', value: '10:15' },
-            { title: 'מנחה מוקדמת', value: '13:20' },
-            { title: 'פ"ש + מסכת בכורות', value: '16:50' },
-            { title: 'מנחה שבת', value: '19:00' },
-            { title: 'ערבית מוצ"ש', value: '20:15' },
-          ],
-          durationSec: 30,
-          type: 'time&text'
-        },
-        {
-          title: 'זמני השבוע',
-          rows: [
-            { title: 'הנץ החמה', value: 'zmanim.sunrise' },
-            { title: 'סוף ק"ש למ"א', value: 'zmanim.sofZmanShmaMGA' },
-            { title: 'סוף ק"ש לגר"א', value: 'zmanim.sofZmanShma' },
-            { title: 'זמן מנחה גדולה', value: 'zmanim.minchaGedola' },
-            { title: 'שקיעת החמה', value: 'zmanim.sunset' },
-            { title: 'צאת הכוכבים', value: 'zmanim.tzeitHakochavim' }
-          ],
-          durationSec: 15,
-          type: 'time&text'
-        },
-        {
-          title: 'talk-tfila.gif',
-          type: 'image',
-          durationSec: 7,
-        },
-      ],
-      footer: 'בית הכנסת רשב"י',
-      showClock: true
-    }
-  },
-  {
     viewName: 'תצוגה לשבת',
     durationSec: 129600,
     viewType: 'two-boards-view',
@@ -103,14 +41,13 @@ export const MOCK_VIEWS_DATA: Array<View> = [
       title: 'calendar.parash',
       rightBoard: [
         {
-          title: 'זמני השבת',
+          title: 'זמני התפילות',
           rows: [
+            { title: 'מנחה ערב שבת', value: '19:35' },
             { title: 'שחרית שבת', value: '08:30' },
-            { title: 'תהילים לילדים', value: '10:15' },
             { title: 'מנחה מוקדמת', value: '13:20' },
-            { title: 'פ"ש + מסכת בכורות', value: '16:50' },
-            { title: 'מנחה שבת', value: '19:00' },
-            { title: 'ערבית מוצ"ש', value: '20:15' },
+            { title: 'מנחה שבת', value: '19:05' },
+            { title: 'ערבית מוצ"ש', value: '20:20' },
             // { title: 'שחרית שבת', value: '08:30' },
             // { title: 'תהילים לילדים', value: '10:15' },
             // { title: 'מנחה מוקדמת', value: '13:20' },
@@ -118,19 +55,21 @@ export const MOCK_VIEWS_DATA: Array<View> = [
             // { title: 'מנחה שבת', value: 'zmanim.minchaShabat' },
             // { title: 'ערבית מוצ"ש', value: 'zmanim.arvitMotash' },
           ],
-          // rows: [
-          //   { title: 'שחרית שבת', value: '08:30' },
-          //   { title: 'תהילים לילדים', value: '10:15' },
-          //   { title: 'מנחה מוקדמת', value: '13:20' },
-          //   { title: 'פ"ש + מסכת בכורות', value: '16:30' },
-          //   { title: 'מנחה שבת', value: '18:40' },
-          //   { title: 'ערבית מוצ"ש', value: '19:50' },
-          // ],
           durationSec: -1,
           type: 'time&text'
         }
       ],
       leftBoard: [
+        {
+          title: 'שיעורי תורה',
+          rows: [
+            { title: 'תהילים לילדים', value: '10:15' },
+            { title: 'פ"ש + מסכת בכורות', value: '16:55' },
+            { title: 'שיעור לנשים בתנ"ך', value: '17:15' },
+          ],
+          durationSec: 30,
+          type: 'time&text'
+        },
         {
           title: 'keep-clean2.png',
           type: 'image',
