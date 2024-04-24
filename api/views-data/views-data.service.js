b79c8437959becc76c5bee007e0e1ccda0ca9478
carmit vaknin <carmit.vk@gmail.com>
2024-04-24 11:44:12 +0300

    passover

--- api/views-data/views-data.service.js
+++ api/views-data/views-data.service.js
@@ -93,6 +93,57 @@ let MOCK_VIEWS_DATA = [
     }
   },
   {
+    viewName: 'תצוגה לחול המועד פסח',
+    showDays:[],
+    showDates:['24042024','25042024','26042024'],
+    viewType: 'two-boards-view',
+    viewFields: {
+      title: 'חול המועד פסח',
+      rightBoard: [
+        {
+          title: ' תפילות חולה"מ',
+          rows: [
+            { title: 'שחרית מנין ראשון', value: '07:00' },
+            { title: 'שחרית מנין שני', value: '08:10' },
+            { title: 'מנחה וערבית', value: 'zmanim.minchaChol' },
+            { title: '----- שיעורי תורה ----- '},
+            { title: 'יום שני - ספר התניא', value: '21:00' },
+            { title: 'יום שלישי - מגילה', value: '21:00' }
+          ],
+          durationSec: -1,
+          type: 'time&text'
+        }
+      ],
+      leftBoard: [
+        {
+          title: 'passover2.jpg',
+          type: 'image',
+          durationSec: 10,
+        },
+        {
+          title: 'keep-clean2.png',
+          type: 'image',
+          durationSec: 7,
+        },
+        {
+          title: 'זמני השבוע',
+          rows: [
+            { title: 'הנץ החמה', value: 'zmanim.sunrise' },
+            { title: 'סוף ק"ש למ"א', value: 'zmanim.sofZmanShmaMGA' },
+            { title: 'סוף ק"ש לגר"א', value: 'zmanim.sofZmanShma' },
+            { title: 'זמן מנחה גדולה', value: 'zmanim.minchaGedola' },
+            { title: 'שקיעת החמה', value: 'zmanim.sunset' },
+            { title: 'צאת הכוכבים', value: 'zmanim.tzeitHakochavim' }
+          ],
+          durationSec: 15,
+          type: 'time&text'
+        },
+      ],
+      footer: 'בית הכנסת רשב"י',
+      showClock: true
+    }
+  },
+  {
     viewName: 'תצוגה לשבת',
     showDays:[5, 6],
     showDates:[],
